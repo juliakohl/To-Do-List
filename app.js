d67bc59540e71e9bc2a8e93e895e43fde1a80010
Julia Kohl <julia.helmine.kohl@gmail.com>
2020-05-01 16:00:38 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useUnifiedTopology: true, useNewUrlParser: true });
+
+const itemSchema = {
+  name: {
+    type: String,
+  }
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const wakeUp = new Item({name: "Wake Up"});
+const brushTeeth = new Item({name: "Brush your teeth"});
+const pants = new Item({name: "Put on some pants"});
+
+const defaultItems = [wakeUp, brushTeeth, pants];
+
+
+
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems){
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if (err){
+          console.log(err);
+        }else{
+          console.log("Successfully added all items");
+        }
+      });
+      res.redirect("/");
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
+});
+
+app.post("/", function(req, res){
+  const itemName = req.body.newItem;
+
+  const item = new Item({name: itemName});
+  item.save();
+  
+  res.redirect("/");
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
